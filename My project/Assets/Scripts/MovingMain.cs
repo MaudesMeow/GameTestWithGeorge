7d629a166a3a0c210652909f1ed4d8a47441aed4
Jordan Hebert <87828418+MaudesMeow@users.noreply.github.com>
2022-10-22 12:23:24 -0500

    updatedMovment

--- My project/Assets/Scripts/MovingMain.cs
+++ My project/Assets/Scripts/MovingMain.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 
 public class MovingMain : MonoBehaviour
 {
-    
+    private Vector2 movement;
+    private Rigidbody2D playerRB;
+
+    public float speed = .02f;
 
 
-    // Update is called once per frame
+    private void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+    }
     void Update()
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        Vector2 moveHorizontal = transform.right * movement.x;
+        Vector2 moveVertical = transform.up * movement.y;
+
+        Vector2 velocity = (((moveHorizontal+moveVertical)).normalized * speed);
+
+        playerRB.MovePosition(playerRB.position + velocity * Time.fixedDeltaTime);
     }
 }
